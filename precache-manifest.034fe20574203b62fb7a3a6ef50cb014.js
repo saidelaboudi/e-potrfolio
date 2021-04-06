31183589a3b1d02f98ba7cc32684cb14f3174d0d
saidelaboudi <sdelaboudi@gmail.com>
2021-04-06 20:53:58 +0100

    Updates

---
+++ precache-manifest.034fe20574203b62fb7a3a6ef50cb014.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fbf31a768028bd1c5add12f37cc41e",
+    "url": "/e-potrfolio/index.html"
+  },
+  {
+    "revision": "e2be2e1b3c760d680eda",
+    "url": "/e-potrfolio/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/e-potrfolio/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/e-potrfolio/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2be2e1b3c760d680eda",
+    "url": "/e-potrfolio/static/js/main.33319bd0.chunk.js"
+  },
+  {
+    "revision": "690c61227f93db7b925f",
+    "url": "/e-potrfolio/static/js/runtime-main.bef44ee3.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/e-potrfolio/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/e-potrfolio/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "beeb892fc7ca42c7f7e47c202aa6cd2a",
+    "url": "/e-potrfolio/static/media/saidelaboudi.beeb892f.jfif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-portfolio/precache-manifest.301d85ab334358021270421152b405cd.js"
+  "/e-potrfolio/precache-manifest.034fe20574203b62fb7a3a6ef50cb014.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/e-potrfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
