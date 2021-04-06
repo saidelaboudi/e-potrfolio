eb515877500a99209382481a2e106c5e3fdb4774
saidelaboudi <sdelaboudi@gmail.com>
2021-04-06 20:45:04 +0100

    Updates

---
+++ precache-manifest.301d85ab334358021270421152b405cd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4318977652e49936e3f4f198fdb7fb",
+    "url": "/e-portfolio/index.html"
+  },
+  {
+    "revision": "bebd1a46bf791797d788",
+    "url": "/e-portfolio/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/e-portfolio/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/e-portfolio/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bebd1a46bf791797d788",
+    "url": "/e-portfolio/static/js/main.c6d9742a.chunk.js"
+  },
+  {
+    "revision": "3ee71f2c257906cad6cb",
+    "url": "/e-portfolio/static/js/runtime-main.6129b6e7.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/e-portfolio/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/e-portfolio/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "beeb892fc7ca42c7f7e47c202aa6cd2a",
+    "url": "/e-portfolio/static/media/saidelaboudi.beeb892f.jfif"
+  }
+]);
